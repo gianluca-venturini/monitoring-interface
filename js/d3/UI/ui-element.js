f8dd8881e56afee723752d310dbc3e5e421b94a8
andreuke <andreatello92@gmail.com>
2015-03-08 23:04:52 -0500

    palette

--- js/d3/UI/ui-element.js
+++ js/d3/UI/ui-element.js
@@ -1,8 +1,23 @@
 var UIElement = function(layer) {
     var self = {};
 
+    // Static attributes
+    UIElement.defaultPalette = {
+        primary: {
+            dark: "#0288D1",
+            normal: "#03A9F4",
+            bright: "#B3E5FC"
+        },
+        accent: {
+            dark: "#F57C00",
+            normal: "#FF9800",
+            bright: "#FFE0B2"
+        }
+    };
+
     // Protected variables
     self._layer = undefined;
+    self.palette = UIElement.defaultPalette;
 
     // Constructor
     self.init = function() {
