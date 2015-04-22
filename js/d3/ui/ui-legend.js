939f73a69333b3a3c307345aa759fa022ef40420
Gianluca Venturini <gianluca.91@gmail.com>
2015-04-22 12:11:19 -0500

    Fixed legend

--- js/d3/ui/ui-legend.js
+++ js/d3/ui/ui-legend.js
@@ -50,6 +50,11 @@ var UILegend = function(delegate, name) {
                 .class("legendGroup")
                 .attr("transform", "translate(" + (windowViewController.width / 2 - UILegend.style.legendBoxWidth - UILegend.style.legendRightMargin) + "," + (windowViewController.height / 2 - UILegend.style.bottomMargin) +")");
         }
+        else {
+            // Move during window resize
+            self._legendGroup
+                .attr("transform", "translate(" + (windowViewController.width / 2 - UILegend.style.legendBoxWidth - UILegend.style.legendRightMargin) + "," + (windowViewController.height / 2 - UILegend.style.bottomMargin) +")");
+        }
 
         if(self._arrowGroup == undefined){
             self._arrowGroup = self._legendGroup
